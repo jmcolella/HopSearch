103a63815c89fba55f7abab06c6a19f67eb95bfa
jmcolella <colella.john@gmail.com>
2016-06-23 10:03:53 -0400

    remove devise from app; will use bcrypt instead

--- Gemfile
+++ Gemfile
@@ -24,11 +24,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # BreweryDB gem
 gem 'brewery_db'
 
-# Devise gem
-gem 'devise'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
