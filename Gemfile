789d3a07e9a9dc638c762f0966536e8c45d21d5a
jmcolella <colella.john@gmail.com>
2016-06-23 17:56:21 -0400

    set up seed file for app and create associations

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # BreweryDB gem
 gem 'brewery_db'
 
+# Faker gem
+gem 'faker'
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  has_many :comments
+  has_many :favorites
+  has_many :favorited_hops, through: :favorites, source: :hop
 end
