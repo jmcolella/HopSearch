a76a5cb10e9737b753908a37910b9bc6a4c70426
jmcolella <colella.john@gmail.com>
2016-07-13 16:34:11 -0400

    add handleChange function to edit comment component

--- app/assets/javascripts/components/edit_comment.js.jsx
+++ app/assets/javascripts/components/edit_comment.js.jsx
@@ -10,15 +10,19 @@ var EditComment = React.createClass({
       this.setState( { comment: response.comment } )
     }.bind(this))
   },
+  handleChange: function(event) {
+    this.setState({comment: event.target.value});
+  },
   render: function() {
     var editCommentURL = "/hops/" + this.props.hop_id + "/comments/" + this.props.comment_id
     var csrfToken = $('meta[name="csrf-token"]').attr('content')
+    var commentBody = this.state.comment.body
     return (
       <div>
         <form action={ editCommentURL } method="post">
           <input type="hidden" name="_method" value="put"/>
           <input type="hidden" name="authenticity_token" value={ csrfToken } />
-          <textarea name="comments[body]" value={ this.state.comment.body }></textarea>
+          <textarea name="comments[body]" value={ commentBody } onChange={ this.handleChange }></textarea>
           <input type="submit" value="edit comment"/>
         </form>
       </div>
