4ca264fa0be84c3b31067b4139f9d2f2108620c5
jmcolella <colella.john@gmail.com>
2016-07-14 12:45:53 -0400

    add header to index component; create index css file and add header image to images folder

--- app/assets/javascripts/components/index.js.jsx
+++ app/assets/javascripts/components/index.js.jsx
@@ -2,13 +2,17 @@ var Index = React.createClass({
   render: function() {
 
     return (
-      <div class="container">
-        <h1>HopSearch</h1>
-
-        { nav }
-
-        <p><a href="/hops">search for hops</a></p>
-      </div>
+      <header>
+        <div className="container header-content">
+          <div className="row header-content-inner">
+            <div className="main-name col-lg-12">
+              <h1>HopSearch</h1>
+              <p>find and learn more about the wild world of hops</p>
+             <button id="header-button" className="btn btn-default" href="#about">learn more</button>
+            </div>
+          </div>
+        </div>
+      </header>
     )
   }
 });
