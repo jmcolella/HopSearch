f242db1bf8b1b9a1ab17812ca7ff706873547f7f
jmcolella <colella.john@gmail.com>
2016-07-19 13:27:40 -0400

    abstract index search form into its own component; create route and controller action for search feature

--- app/controllers/hops_controller.rb
+++ app/controllers/hops_controller.rb
@@ -4,6 +4,10 @@ class HopsController < ApplicationController
     @hops = Hop.all
   end
 
+  def search
+    binding.pry
+  end
+
   def show
     @hop = Hop.find_by(id: params[:id])
     @comments = @hop.comments
