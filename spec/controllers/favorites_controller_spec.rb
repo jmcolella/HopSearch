d2d62952a7071aff3fd7c56f4464937c21fc9d66
jmcolella <colella.john@gmail.com>
2016-07-25 19:02:44 -0400

    complete Favorite controller rspec tests

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe FavoritesController, type: :controller do
 
-  let(:hop) {Hop.create}
-  let(:user) {User.create(first_name: "John", last_name: "Smith", email: "jsmith@gmail.com", password: "password")}
-  let(:favorite) {Favorite.create(hop_id: hop.id, user_id: user.id)}
+  let(:hop) {FactoryGirl.create(:hop)}
+  let(:user) {User.find_by(id: 48)}
+  let(:hop_favorite) {Hop.find_by(id: 47)}
+  let(:favorite) {Favorite.find_by(hop_id: hop_favorite.id, user_id: user.id)}
 
   describe "POST #create" do
 
     it "finds the hop and assigns it as @hop" do
-      post :create, {hop_id: hop.id}
+      post :create, {hop_id: hop.id, user_id: user.id}
       expect(assigns(:hop)).to eq(hop)
     end
 
@@ -20,12 +21,12 @@ RSpec.describe FavoritesController, type: :controller do
 
     it "creates a new favorite record" do
       expect{
-        post :create, {hop_id: hop.id}
+        post :create, {hop_id: hop.id, user_id: user.id}
       }.to change(Favorite, :count).by(1)
     end
 
     it "redirects to the hop show page" do
-      post :create, {hop_id: hop.id}
+      post :create, {hop_id: hop.id, user_id: user.id}
       expect(response).to redirect_to(hop)
     end
 
@@ -34,21 +35,24 @@ RSpec.describe FavoritesController, type: :controller do
   describe "DELETE #destroy" do
 
     it "finds the hop and assigns it as @hop" do
-      delete :destroy, {hop_id: hop.id, user_id: user.id}
-      expect(assigns(:hop)).to eq(hop)
+      delete :destroy, {hop_id: hop_favorite.id, user_id: user.id}
+      expect(assigns(:hop)).to eq(hop_favorite)
     end
 
     it "finds the user and assigns it as @user" do
-      delete :destroy, {hop_id: hop.id, user_id: user.id}
+      delete :destroy, {hop_id: hop_favorite.id, user_id: user.id}
       expect(assigns(:user)).to eq(user)
     end
 
-    it "creates a new favorite record" do
-      pending
+    it "removes the favorite record from the databse" do
+      id = favorite.id
+      delete :destroy, {hop_id: hop_favorite.id, user_id: user.id}
+      expect(Favorite.where(id: id)).not_to exist
     end
 
     it "redirects to the hop show page" do
-      pending
+      delete :destroy, {hop_id: hop_favorite.id, user_id: user.id}
+      expect(response).to redirect_to(hop_favorite)
     end
 
   end
