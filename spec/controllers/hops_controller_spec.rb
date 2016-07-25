2adce13aa55f2f3abd03a791d4013a5a04074bb9
jmcolella <colella.john@gmail.com>
2016-07-25 13:12:26 -0400

    complete Hops controller tests

--- spec/controllers/hops_controller_spec.rb
+++ spec/controllers/hops_controller_spec.rb
@@ -2,11 +2,51 @@ require 'rails_helper'
 
 RSpec.describe HopsController, type: :controller do
 
-   describe "GET #index" do
-      it "assigns all hops as @hops" do
-        get :index
-        expect(assigns(:hops)).to eq(Hop.all)
-      end
+  let(:hop) { Hop.find_by(id: 1)}
+
+  describe "GET #index" do
+
+    it "assigns @hops" do
+      hop = Hop.create
+      get :index
+      expect(assigns(:hops)).to eq([hop])
     end
+
+    it "renders new template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+  end
+
+  describe "GET #show" do
+    let(:hop) {Hop.create}
+    it "assigns a hop to @hop" do
+      get :show, {id: hop.id}
+      expect(assigns(:hop)).to eq(hop)
+    end
+
+    it "renders show template" do
+      get :show, {id: hop.id}
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "GET #search" do
+
+    let(:hop_to_find) { Hop.create(name:"Galaxy") }
+
+    it "assigns an array to @search_results" do
+      search = { hop: hop_to_find }
+      get :search, {search: search}
+      expect(assigns(:search_results)).to eq([])
+    end
+
+    it "renders search template" do
+      search = { hop: hop_to_find }
+      get :search, {search: search}
+      expect(response).to render_template("search")
+    end
+  end
 
 end
