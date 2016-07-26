167cd5fe884a81727c0031c57b0a4d6bf5e8abbc
jmcolella <colella.john@gmail.com>
2016-07-26 12:14:11 -0400

    complete Favorite model rspec test

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Favorite model associations" do
+
+    it { should belong_to(:user) }
+
+    it { should belong_to(:hop) }
+
+  end
+
 end
