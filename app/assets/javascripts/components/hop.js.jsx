b006c5c9b4a54b7b8de33a0972c353cf47ae54f4
jmcolella <colella.john@gmail.com>
2016-07-27 17:29:34 -0400

    style hop index; add flex box styling to body tag

--- app/assets/javascripts/components/hop.js.jsx
+++ app/assets/javascripts/components/hop.js.jsx
@@ -3,7 +3,7 @@ var Hop = React.createClass({
     var url = "/hops/" + this.props.data.id
     return (
       <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12 individual-hop-container">
-        <h2> <a href={ url }> { this.props.data.name }</a> </h2>
+        <h2> <a className="hop-link" href={ url }> { this.props.data.name }</a> </h2>
       </div>
     )
   }
