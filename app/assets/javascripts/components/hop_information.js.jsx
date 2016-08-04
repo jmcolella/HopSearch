e3adc8e51855a76b240ca10d645b16193bdb98f0
jmcolella <colella.john@gmail.com>
2016-08-04 16:50:36 -0400

    handle situation where alpha acid or beta acid is null and place 'N/A' in its place

--- app/assets/javascripts/components/hop_information.js.jsx
+++ app/assets/javascripts/components/hop_information.js.jsx
@@ -8,6 +8,17 @@ var HopInformation = React.createClass({
         var favoriteButton =
           <FavoriteButton current_user = { this.props.current_user } hop = { hop } />
     }
+
+    if ( hop.alpha_acid === null ) {
+      var alphaAcid = "N/A"
+    } else {
+      var alphaAcid = hop.alpha_acid
+    }
+    if ( hop.beta_acid === null ) {
+      var betaAcid = "N/A"
+    } else {
+      var betaAcid = hop.alpha_acid
+    }
     return (
       <div className="hop-container col-lg-12">
         <h1>{ hop.name }</h1>
@@ -20,8 +31,8 @@ var HopInformation = React.createClass({
         <div className="hop-information-list">
           <ul>
             <li>{ hop.country_of_origin }</li>
-            <li>Alpha Acid: { hop.alpha_acid }%</li>
-            <li>Beta Acid: { hop.beta_acid }%</li>
+            <li>Alpha Acid: { alphaAcid }%</li>
+            <li>Beta Acid: { betaAcid }%</li>
           </ul>
         </div>
       </div>
