7efca352c5d6a8803d49f9ca948f07dc42462840
jmcolella <colella.john@gmail.com>
2016-08-04 16:45:50 -0400

    add styling for no post comment message and precompile assets

--- app/assets/javascripts/components/no_post_comment.js.jsx
+++ app/assets/javascripts/components/no_post_comment.js.jsx
@@ -2,7 +2,7 @@ var NoPostComment = React.createClass({
   render: function() {
     return (
       <div>
-        <p>Please <a id="login-link" href="/login">login</a> or <a id="sign-up-link" href="/users/new">sign up</a> to leave a comment.</p>
+        <p id="no-post-message">Please <a id="login-link" href="/login">login</a> or <a id="sign-up-link" href="/users/new">sign up</a> to leave a comment.</p>
       </div>
     )
   }
