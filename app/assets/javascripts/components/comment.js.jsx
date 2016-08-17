52a0b93af19d4d9a111d941082fd1a212a8e948e
jmcolella <colella.john@gmail.com>
2016-08-17 15:41:13 -0400

    add a comment options component to render for a user comment

--- app/assets/javascripts/components/comment.js.jsx
+++ app/assets/javascripts/components/comment.js.jsx
@@ -7,24 +7,18 @@ var Comment = React.createClass({
     var csrfToken = $('meta[name="csrf-token"]').attr('content')
     var deleteURL = "/hops/" + data.hop.id + "/comments/" + data.comment_id
     if( current_user && data.comment_user.id === current_user.id ) {
-      var editComment =
-        <div>
-          <li><a className="user-edit-comment-link" href={ editURL }>edit comment</a></li>
-          <li>
-            <form action={ deleteURL } method="post">
-              <input type="hidden" name="_method" value="delete"/>
-              <input type="hidden" name="authenticity_token" value={ csrfToken }/>
-              <input className="user-delete-comment-btn" type="submit" value="delete comment"/>
-            </form>
-          </li>
-        </div>
+      var commentOptions =
+        <CommentOptions
+            editURL={editURL}
+            deleteURL={deleteURL}
+            csrfToken={csrfToken} />
     }
     return (
       <div className="comment-body col-lg-12">
         <p className="comment-text">{ data.body }</p>
         <ul className="comment-body-info">
           <li><a className="user-comment-link" href={"/users/" + data.comment_user.id}>{ data.comment_user.first_name } { data.comment_user.last_name }</a></li>
-          { editComment }
+          { commentOptions }
         </ul>
       </div>
     )
